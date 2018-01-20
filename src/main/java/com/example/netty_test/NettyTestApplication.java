1e7fc814f241474ee904f3281b7f9791aa25b900
xiazl <xiazl@shishike.com>
2018-01-20 22:44:42 +0800

    demo bak

---
+++ src/main/java/com/example/netty_test/NettyTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.netty_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/netty_test/NettyTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.netty_test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NettyTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
